e7570b1acb9da38e1fc60e53fdfc4e8b2421350d
Huang <jjj08254@gmail.com>
2020-05-08 21:21:49 -0400

    add eslint and prettier to the project

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,17 +1,17 @@
 import React from 'react';
 
-class StorePicker extends React.Component{
+class StorePicker extends React.Component {
   render() {
-      return (
-          // <React.Fragment>
-          // <p>Fragment allows multiple elements</p>
-          <form className='store-selector'>
-              <h2>Please Enter A Store</h2>
-              <input type='text' required placeholder='Store Name' />
-              <button type='submit'>Visit Store -></button>
-          </form>
-          // </React.Fragment>
-      )
+    return (
+      // <React.Fragment>
+      // <p>Fragment allows multiple elements</p>
+      <form className="store-selector">
+        <h2>Please Enter Add Store</h2>
+        <input type="text" required placeholder="Store Name" />
+        <button type="submit">Visit Store -></button>
+      </form>
+      // </React.Fragment>
+    );
   }
 }
 
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import {render} from 'react-dom';
+import { render } from 'react-dom';
 import StorePicker from './components/StorePicker.js';
 import './css/style.css';
-
 
 render(<StorePicker />, document.querySelector('#main'));
